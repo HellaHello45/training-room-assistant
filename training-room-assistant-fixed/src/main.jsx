bfa7cf50af95fb40babd6f13fdddf4cf7c1c7e73
HellaHello45 <hellahello014@gmail.com>
2025-06-28 14:29:37 +0630

    Delete training-room-assistant-fixed directory

--- training-room-assistant-fixed/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TrainingRoomAssistant from './TrainingRoomAssistant'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TrainingRoomAssistant />
-  </React.StrictMode>,
-)
